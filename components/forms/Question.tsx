57cd697018b75f5f45bc4637105662ab4527994e
chekirko <victorche59@gmail.com>
2024-01-25 17:17:24 +0200

    add reusability of the form

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -18,11 +18,14 @@ import {
 import { Badge } from "../ui/badge";
 import { Input } from "@/components/ui/input";
 import { Button } from "../ui/button";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Image from "next/image";
+
+const type: any = "create";
 
 const Question = () => {
   const editorRef = useRef({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof QuestionsSchema>>({
@@ -36,9 +39,16 @@ const Question = () => {
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof QuestionsSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+    setIsSubmitting(true);
+
+    try {
+      // make an async call to your API -> create a question
+      // contain all form data
+      // navigate to home page
+    } catch (error) {
+    } finally {
+      setIsSubmitting(false);
+    }
   }
 
   const handleInputKeyDown = (
@@ -204,7 +214,17 @@ const Question = () => {
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <Button
+          type="submit"
+          className="primary-gradient w-fit !text-light-900"
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? (
+            <>{type === "edit" ? "Editing..." : "Posting..."}</>
+          ) : (
+            <>{type === "edit" ? "Edit Question" : "Ask a Question"}</>
+          )}
+        </Button>
       </form>
     </Form>
   );
