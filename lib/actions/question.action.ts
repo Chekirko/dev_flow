0b3facb8d9623354c27e8bfef54c747596ea00ee
chekirko <victorche59@gmail.com>
2024-02-28 14:28:24 +0200

    add main part of page

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -3,7 +3,11 @@
 import Question from "@/database/question.model";
 import { connectToDb } from "../mongoose";
 import Tag from "@/database/tag.model";
-import { CreateQuestionParams, GetQuestionsParams } from "./shared.types";
+import {
+  CreateQuestionParams,
+  GetQuestionByIdParams,
+  GetQuestionsParams,
+} from "./shared.types";
 import { revalidatePath } from "next/cache";
 import User from "@/database/user.model";
 
@@ -17,6 +21,26 @@ export async function getQuestions(params: GetQuestionsParams) {
       .sort({ createdAt: -1 });
 
     return { questions };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getQuestionById(params: GetQuestionByIdParams) {
+  try {
+    connectToDb();
+
+    const { questionId } = params;
+    const question = await Question.findById(questionId)
+      .populate({ path: "tags", model: Tag, select: "_id name" })
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id clerkId name picture",
+      });
+
+    return question;
   } catch (error) {
     console.log(error);
     throw error;
