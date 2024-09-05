a437d65a1741a042757a0fad30337d11e4620145
chekirko <victorche59@gmail.com>
2024-09-05 23:47:40 +0300

    add upvote and downvote actions

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -7,6 +7,7 @@ import {
   CreateQuestionParams,
   GetQuestionByIdParams,
   GetQuestionsParams,
+  QuestionVoteParams,
 } from "./shared.types";
 import { revalidatePath } from "next/cache";
 import User from "@/database/user.model";
@@ -75,6 +76,74 @@ export async function createQuestion(params: CreateQuestionParams) {
     await Question.findByIdAndUpdate(question._id, {
       $push: { tags: { $each: tagDocuments } },
     });
+
+    revalidatePath(path);
+  } catch (error) {}
+}
+
+export async function upvoteQuestion(params: QuestionVoteParams) {
+  try {
+    connectToDb();
+    const { questionId, userId, hasupVoted, hasdownVoted, path } = params;
+
+    let updateQuery;
+
+    if (hasupVoted) {
+      updateQuery = {
+        $pull: { upvotes: userId },
+      };
+    } else if (hasdownVoted) {
+      updateQuery = {
+        $pull: { downvotes: userId },
+        $push: { upvotes: userId },
+      };
+    } else {
+      updateQuery = {
+        $addToSet: { upvotes: userId },
+      };
+    }
+
+    const question = await Question.findByIdAndUpdate(questionId, updateQuery, {
+      new: true,
+    });
+
+    if (!question) {
+      throw new Error("Question not found");
+    }
+
+    revalidatePath(path);
+  } catch (error) {}
+}
+
+export async function downvoteQuestion(params: QuestionVoteParams) {
+  try {
+    connectToDb();
+    const { questionId, userId, hasupVoted, hasdownVoted, path } = params;
+
+    let updateQuery;
+
+    if (hasdownVoted) {
+      updateQuery = {
+        $pull: { downvotes: userId },
+      };
+    } else if (hasupVoted) {
+      updateQuery = {
+        $pull: { upvotes: userId },
+        $push: { downvotes: userId },
+      };
+    } else {
+      updateQuery = {
+        $addToSet: { downvotes: userId },
+      };
+    }
+
+    const question = await Question.findByIdAndUpdate(questionId, updateQuery, {
+      new: true,
+    });
+
+    if (!question) {
+      throw new Error("Question not found");
+    }
 
     revalidatePath(path);
   } catch (error) {}
