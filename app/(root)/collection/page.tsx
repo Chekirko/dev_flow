7a20cd23e079d8f50526705752132af9d213ff1e
chekirko <victorche59@gmail.com>
2024-10-19 20:28:25 +0300

    integrate filters on collections page

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -18,6 +18,7 @@ export default async function CollectionPage({
   const result = await getSavedQuestions({
     clerkId: userId,
     searchQuery: searchParams.q,
+    filter: searchParams.filter,
   });
 
   return (
